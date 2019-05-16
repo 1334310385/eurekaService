e066504601d8899ccd0ed7fdb9114b6de29f543e
zhaoshangkun <1334310385@qq.com>
2019-05-16 17:44:22 +0800

    第一版

---
+++ src/main/java/com/eurekaService/Controller/QueryController.java
@@ -0,0 +1,22 @@
+package com.eurekaService.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/query")
+public class QueryController {
+
+	@Autowired
+	EurekaClientConfigBean eurekaClientConfigBean;
+	
+	@GetMapping("/eureka-server")
+	public Object getEurekaServerUrl() {
+		return eurekaClientConfigBean.getServiceUrl();
+	}
+}
